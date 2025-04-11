fc3152d3bb6c49ad6016073641ecb55c8e5e98f0
herodmfrancato <francatoherod@gmail.com>
2025-04-10 22:11:14 -0300

    Estruturando o código e adicionando cenario de compra de produtos

--- cypress/e2e/login/comprandoProdutos.cy.js
+++ cypress/e2e/login/comprandoProdutos.cy.js
@@ -7,12 +7,18 @@ describe('Realizando compra no site com sucesso', () => {
         cy.get('[data-test="password"]').type("secret_sauce")
         cy.get('#login-button').click()
         cy.get('.product_label').should('contain', 'Products')
-    });
-    it('Deve adicionar produto no carrinho e finalizar compra', () => {
-        cy.visit("https://www.saucedemo.com/v1/")
-        cy.get('[data-test="username"]').type("standard_user")
-        cy.get('[data-test="password"]').type("secret_sauce")
-        cy.get('#login-button').click()
+        cy.get('.product_sort_container').select('Price (low to high)')
+        cy.get('#item_2_title_link > .inventory_item_name').should('contain', 'Sauce Labs Onesie')
         cy.get(':nth-child(1) > .pricebar > .btn_primary').click()
+        cy.get('#item_0_title_link > .inventory_item_name').should('contain', 'Sauce Labs Bike Light')
+        cy.get(':nth-child(2) > .pricebar > .btn_primary').click()
+        cy.get('svg[data-icon="shopping-cart"]').parent().click()
+        cy.get('.btn_action').click()
+        cy.get('[data-test="firstName"]').type("Test")
+        cy.get('[data-test="lastName"]').type("Test")
+        cy.get('[data-test="postalCode"]').type("10101010")
+        cy.get('.btn_primary').click()
+        cy.get('.btn_action').click()
+        cy.get('.complete-header').should('contain', 'THANK YOU FOR YOUR ORDER')
     });
 });
