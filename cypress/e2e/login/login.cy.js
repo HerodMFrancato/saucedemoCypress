944d52f0e423fc83be3ade18cfb2325c3d90f82e
herodmfrancato <francatoherod@gmail.com>
2025-04-14 09:30:04 -0300

    Refatorando código com commands

--- cypress/e2e/login/login.cy.js
+++ cypress/e2e/login/login.cy.js
@@ -2,34 +2,17 @@
 
 describe('Testes Funcional de Login', () => {
     it('Deve realizar o login com sucesso', () => {
-        cy.visit("https://www.saucedemo.com/v1/")
-        cy.get('[data-test="username"]').type("standard_user")
-        cy.get('[data-test="password"]').type("secret_sauce")
-        cy.get('#login-button').click()
+        cy.login_teste('standard_user', 'secret_sauce')
         cy.get('.product_label').should('contain', 'Products')
     });
 
-    it('Validando login incorreto', () => {
-        cy.visit("https://www.saucedemo.com/v1/")
-        cy.get('[data-test="username"]').type("standard")
-        cy.get('[data-test="password"]').type("secret_sauce")
-        cy.get('#login-button').click()
+    it('Validando usuario incorreto', () => {
+        cy.login_teste('standard_user1', 'secret_sauce')
         cy.get('[data-test="error"]').should('contain', 'Epic sadface: Username and password do not match any user in this service')
     });
 
     it('Validando senha incorreta', () => {
-        cy.visit("https://www.saucedemo.com/v1/")
-        cy.get('[data-test="username"]').type("standard_user")
-        cy.get('[data-test="password"]').type("secret")
-        cy.get('#login-button').click()
+        cy.login_teste('standard_user', 'secret_sauce1')
         cy.get('[data-test="error"]').should('contain', 'Epic sadface: Username and password do not match any user in this service')
-    });
-
-    it('Adicionar produto no carrinho', () => {
-        cy.visit("https://www.saucedemo.com/v1/")
-        cy.get('[data-test="username"]').type("standard_user")
-        cy.get('[data-test="password"]').type("secret_sauce")
-        cy.get('#login-button').click()
-        cy.get(':nth-child(1) > .pricebar > .btn_primary').click()
     });
 });
